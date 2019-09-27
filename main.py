0326c463cef11d34796436cfd71bcc6f36a55d29
hnlatha <lathahn.ece@bmsce.ac.in>
2019-09-27 13:54:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ from model import generator_model, discriminator_model, generator_containing_dis
 
 import os
 #os.mkdir('/content/ImageDeblurring/result')
-#os.mkdir('/content/ImageDeblurring/result/interim_300')
-
-#os.mkdir('/content/ImageDeblurring/result/test_300')
-#os.mkdir('/content/ImageDeblurring/result/finally_300')
-#os.mkdir('/content/ImageDeblurring/weight')
+os.mkdir('/content/ImageDeblurring/result/interim_300')
+os.mkdir('/content/ImageDeblurring/result/test_300')
+os.mkdir('/content/ImageDeblurring/result/finally_300')
+os.mkdir('/content/ImageDeblurring/weight')
 
 
 def train(batch_size, epoch_num):
